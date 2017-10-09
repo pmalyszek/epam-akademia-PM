2df1969bc29ccefdd8c256936bd8ae7ecce97821
sartrek <sartrek@wp.pl>
2017-10-09 10:53:24 +0200

    Struktura projektu, krok 1: polecenie dir

--- src/com/epam/javaacademy/Shell.java
+++ src/com/epam/javaacademy/Shell.java
@@ -1,5 +1,6 @@
 package com.epam.javaacademy;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Shell {
@@ -7,10 +8,52 @@ public class Shell {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner=new Scanner(System.in);
+		String url="C:\\\\Users\\Przemek\\workspace\\epam-akademia";
 		while(true){
 			System.out.print("$>");
 			String userInput=scanner.nextLine();
-			System.out.println("User said: "+userInput);
+			switch(userInput){
+			case "dir":
+				File directory=new File(url);
+				File[] fileList=directory.listFiles();
+				
+				for(File f:fileList){
+					if(f.isDirectory()){
+						System.out.print("DIR");
+					}
+					else{
+						System.out.print("FIL");
+					}
+					System.out.print("\t"+f.getName()+"\n");
+				}
+				
+				System.out.println("User said: "+userInput);
+				break;
+			
+			case "cd":
+				System.out.println("User said: "+userInput);
+				break;
+			
+			case "prompt":
+				System.out.println("User said: "+userInput);
+				break;
+				
+			case "tree":
+				System.out.println("User said: "+userInput);
+				break;
+			
+			case "exit":
+				System.out.println("User said: "+userInput);
+				break;
+			
+			case "statistics":
+				System.out.println("User said: "+userInput);
+				break;
+			default: 
+				System.out.println(userInput+": unknown commnd");
+				break;
+			}
+			
 		}
 	}
 
