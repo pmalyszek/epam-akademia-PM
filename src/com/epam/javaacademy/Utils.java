8dfaac0478160ec945c7e5849d7bbb46bab4c05d
sartrek <sartrek@wp.pl>
2017-10-09 13:26:57 +0200

    Kroki 4,5,6- polecenia tree, exit, statistics - niepelne

--- src/com/epam/javaacademy/Utils.java
+++ src/com/epam/javaacademy/Utils.java
@@ -1,7 +1,12 @@
 package com.epam.javaacademy;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Scanner;
+
 
 public class Utils {
 	
@@ -10,6 +15,7 @@ public class Utils {
 	String command;	
 	String parameter;
 	String prompt="$>";
+	HashMap<String, Integer> sCounter;
 	boolean isPromptCwd=false;
 	private Input input;
 	
@@ -26,6 +32,13 @@ public class Utils {
 			parameter=null;
 		}
 		this.input=new Input(command,parameter);	
+		sCounter=new HashMap<>();
+		sCounter.put("dir", 0);
+		sCounter.put("cd", 0);
+		sCounter.put("prompt", 0);
+		sCounter.put("tree", 0);
+		sCounter.put("statistics", 0);
+		//sCounter.replace(command, 1);
 	}
 	public void setCommand(String com){
 		input.setCommand(com);
@@ -58,16 +71,14 @@ public class Utils {
 				System.out.print("\t"+f.getName()+"\n");
 			}
 			
-			//System.out.println("User said: "+userInput);
 			break;
 		
 		case "cd":
 			url=changePath(url, parameter);
 			if(isPromptCwd)
 				prompt=changePrompt(url,"$cwd");
-			print();
-			//System.out.println(url);
-			//System.out.println("User said: "+userInput);
+			//System.out.print(prompt);
+			
 			break;
 		
 		case "prompt":
@@ -76,25 +87,50 @@ public class Utils {
 			break;
 			
 		case "tree":
+			
+			File path=new File(url);
+			listFileTree(path,1);
+			
 			System.out.println("User said: ");
 			break;
 		
 		case "exit":
-			System.out.println("User said: ");
-			
+			System.out.println("Bye");
+			System.exit(0);
 			break;
 		
 		case "statistics":
-			System.out.println("User said: ");
+			Iterator it=sCounter.entrySet().iterator();
+			while(it.hasNext()){
+				Map.Entry pair=(Map.Entry)it.next();
+				System.out.println(pair.getKey()+":"+pair.getValue());
+			}
+			
 			break;
 		default: 
 			System.out.println(input.getCommand()+": unknown commnd");
 			break;
 		}
+		sCounter.replace(command,sCounter.get(command).intValue()+1 );
 	}
+		
 	
-	
-	
+	private void listFileTree(File path, int count) {
+
+		File[] listPath=path.listFiles();
+		for(File f:listPath){
+			if(!f.isDirectory())
+			{
+				for(int i=0;i<count;i++)
+				System.out.print("-");
+				System.out.print(f.getName()+"\n");
+			}
+				
+			else{
+				listFileTree(f,count+1);
+			}
+		}		
+	}
 	private String changePrompt(String url, String param) {
 		
 		if(param.equals("$cwd"))
@@ -108,7 +144,8 @@ public class Utils {
 	}
 	private static String changePath(String url, String parameter) {
 
-		if(parameter.equals("..")){
+		if(parameter!=null)
+		{if(parameter.equals("..")){
 			url=url.substring(0, url.lastIndexOf("\\"));
 		}
 		else
@@ -120,7 +157,7 @@ public class Utils {
 			}
 			url=url.concat("\\");
 			url=url.concat(parameter);			
-		}
+		}}
 		return url;
 	}
 	
@@ -134,6 +171,7 @@ public class Utils {
 			parameter=null;
 		}
 		input.setCommand(command);
+	
 		
 		
 	}
